20ce3c098df5f3bd3a78e83c61993e5604bc7946
reactivegeeks <software.dev0218@gmail.com>
2017-03-17 15:20:24 -0600

    search by keyword

--- src/routes/Dashboard/GoogleResults/components/GoogleResults.js
+++ src/routes/Dashboard/GoogleResults/components/GoogleResults.js
@@ -1,8 +1,7 @@
 import React from 'react';
 //import uid from 'node-uuid';
-//import _ from 'underscore';
-//import numeral from 'numeral';
 import { Link } from 'react-router';
+
 import {
     Row,
     Col,
@@ -13,7 +12,10 @@ import {
     Divider
 } from 'components';
 
-import { SearchInput } from './SearchInput';
+import SearchInput from './SearchInput';
+import SearchResultTypes from './SearchResultTypes';
+import SearchKeywords from './SearchKeywords';
+import SearchPeriod from './SearchPeriod';
 
 import Spinner from 'components/Spinner';
 import GoogleResult from './GoogleResult';
@@ -22,9 +24,15 @@ import { CONTENT_VIEW_STATIC } from 'layouts/DefaultLayout/modules/layout';
 import { Colors } from 'consts';
 
 
-const GoogleResults = ({ results, keywords, isFetching }) => {
+const GoogleResults = ({ results, keywords, getResults }) => {
   return (
     <Row>
+      <Col lg={ 2 }>
+        <SearchKeywords
+          keywords={keywords}
+          getResults={getResults}
+        />
+      </Col>
       <Pagination
         bsSize="medium"
         items={3}
