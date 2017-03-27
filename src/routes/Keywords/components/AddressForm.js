62cf616fedb6cb1b55944427c41094b4f9e2c93b
reactivegeeks <software.dev0218@gmail.com>
2017-03-27 11:28:07 -0600

    add drop down for states

--- src/routes/Keywords/components/AddressForm.js
+++ src/routes/Keywords/components/AddressForm.js
@@ -1,9 +1,20 @@
 import React from 'react';
+const { DOM: { select } } = React;
 
-import { FormGroup } from 'components';
+import { FormGroup, FormControl } from 'components';
 import classes from './keywords.scss';
+import states from 'utils/states';
 
-const AddressForm = ({ fields, renderField, handleSubmit, renderNextForm, invalid, submitting}) => {
+const dropDownSelect = ({ input }) => (
+  <FormControl {...input} componentClass='select'>
+    <option value=''>Select a state...</option>
+    {states.map(state =>
+                <option value={state} key={state}>{state}</option>
+                )
+    }
+  </FormControl>
+)
+const AddressForm = ({ Field, fields, renderField, handleSubmit, renderNextForm, invalid, submitting}) => {
   return (
     <form onSubmit={handleSubmit(renderNextForm)}>
       <FormGroup>
@@ -22,9 +33,12 @@ const AddressForm = ({ fields, renderField, handleSubmit, renderNextForm, invali
         <label>
           State
         </label>
-        {renderField(fields.state)}
-      </FormGroup>
-      <FormGroup>
+          <Field
+            name='state'
+            component={dropDownSelect}
+          />
+        </FormGroup>
+        <FormGroup>
         <label>
           Zipcode
         </label>
