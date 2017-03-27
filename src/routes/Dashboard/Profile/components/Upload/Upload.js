fdfbe386b1f2e0cea32580de6e4ea491893fcebe
reactivegeeks <software.dev0218@gmail.com>
2017-03-27 16:48:34 -0600

    render error if not image

--- src/routes/Dashboard/Profile/components/Upload/Upload.js
+++ src/routes/Dashboard/Profile/components/Upload/Upload.js
@@ -8,25 +8,31 @@ import classes from './Upload.scss';
 
 const Upload = ({ name, input, onImageUpload, avatar }) => {
   if(!avatar) {
-  return (
-    <Dropzone className={`text-center ${classes.uploadPanel}`}
-      name={name}
-      onDrop={onImageUpload}
-    >
-      <Panel >
-        <p>
-          <i className="fa fa-3x fa-user text-gray-light m-y-2"></i>
-        </p>
-        <h5>Upload Your Avatar</h5>
-        <p>
-          Drag a file here or <a href="javascript:;" onClick={onImageUpload}> browse</a> for a file to upload.
-        </p>
-        <p className='small'>
-          JPG, GIF, PNG, MOV and AVI. Please choose a files under 2GB to upload. File sizes are 400 x 300px.
-        </p>
-      </Panel>
-    </Dropzone>
-  )
+    return (
+      <Dropzone className={`text-center ${classes.uploadPanel}`}
+        name={name}
+        onDrop={onImageUpload}
+        accept='image/*'
+      >
+        {({isDragRejected, acceptedFiles, rejectedFiles }) => {
+          return (
+            <div>
+              {rejectedFiles.length > 0 && <p className='text-danger'>File must be an image</p> }
+              <p>
+                <i className="fa fa-3x fa-user text-gray-light m-y-2"></i>
+              </p>
+              <h5>Upload Your Avatar</h5>
+              <p>
+                Drag a file here or <a href="javascript:;" onClick={onImageUpload}> browse</a> for a file to upload.
+              </p>
+              <p className='small'>
+                JPG and PNG. Please choose a files under 2GB to upload. File sizes are 400 x 300px.
+              </p>
+            </div>
+            )
+        }}
+      </Dropzone>
+    )
   } else {
     return (
       <Panel className={`text-center ${classes.uploadPanel}`}>
