1752977c50426eee16ca05fbe07869a629cd5e73
reactivegeeks <software.dev0218@gmail.com>
2017-04-14 15:58:57 -0600

    fix case sensitivity

--- src/routes/client/Keywords/components/Keywords.js
+++ src/routes/client/Keywords/components/Keywords.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { connect } from 'routes/RoutedComponent';
 import { Link } from 'react-router';
 
 import {
