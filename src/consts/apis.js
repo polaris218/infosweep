2afedc76edd74ec4e40b8e49d219dc19ff16bc9c
reactivegeeks <software.dev0218@gmail.com>
2017-04-20 13:33:11 -0600

    dynamic api url based on process.env.NODE_ENV

--- src/consts/apis.js
+++ src/consts/apis.js
@@ -1,3 +1,6 @@
-//export const BASE_URL = 'http://api.blitzmonitoring.com/dashboard/api/v1';
-//export const BASE_URL = 'http://localhost:3000/dashboard/api/v1';
-export const BASE_URL = 'http://localhost:3000';
+const API_URL = {
+  'development': 'http://localhost:3000',
+  'production' : 'http://api.blitzmonitoring.com'
+}
+
+export const BASE_URL = API_URL[process.env.NODE_ENV]
