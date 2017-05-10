fd590ee9708a4df5b0fea331fdcaef8821410884
reactivegeeks <software.dev0218@gmail.com>
2017-05-10 10:00:47 -0600

    replace lighting bolt with ship icon

--- src/routes/auth/Login/components/Login.js
+++ src/routes/auth/Login/components/Login.js
@@ -66,7 +66,7 @@ const Login = ({ errorMessage, submitForm }) => {
                 <p className='text-center text-gray-light'>
                   <strong>Blitz Monitoring </strong>
                   <span className='text-gray-light'>
-                    © 2009 - 2017. Made by <i className="fa fa-fw fa-flash text-primary"></i> Denver, CO
+                    © 2009 - 2017. Made by <i className="fa fa-fw fa-ship text-primary"></i> Denver, CO
                   </span>
                 </p>
               </Col>
