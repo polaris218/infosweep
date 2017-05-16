fdc77922bb67ee4b97d7cb070003fafe43452a59
reactivegeeks <software.dev0218@gmail.com>
2017-05-16 09:20:08 -0600

    add https to api

--- src/consts/apis.js
+++ src/consts/apis.js
@@ -1,6 +1,6 @@
 const API_URL = {
   'development': 'http://localhost:3000',
-  'production' : 'http://api.blitzmonitoring.com'
+  'production' : 'https://api.blitzmonitoring.com'
 }
 
 export const BASE_URL = API_URL[process.env.NODE_ENV]
