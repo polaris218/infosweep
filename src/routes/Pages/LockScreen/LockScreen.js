3000463b589ba3e9c6b13b6944637241b9b5cb42
reactivegeeks <software.dev0218@gmail.com>
2017-06-15 23:05:55 -0400

    remove all instances of SPIN

--- src/routes/Pages/LockScreen/LockScreen.js
+++ src/routes/Pages/LockScreen/LockScreen.js
@@ -82,7 +82,7 @@ class LockScreenContainer extends RoutedComponent {
                                 </Form>
                             </Panel>
                             <p className='text-center text-gray-light'>
-                                <strong>SPIN Dashboard </strong>
+                                <strong>Clickadilly</strong>
                                 <span className='text-gray-light'>
                                     © 2009 - 2016. Made by <i className="fa fa-fw fa-heart text-danger"></i> New York, US
                                 </span>
