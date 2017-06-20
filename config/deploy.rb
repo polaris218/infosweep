db1a6688d07c50c4d47eca85c40d16ad7bd73f11
reactivegeeks <software.dev0218@gmail.com>
2017-06-20 11:43:28 -0600

    comment out capLock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.8.1"
+#lock "3.8.1"
 
 set :application, "blitz-frontend"
 set :repo_url, "https://4e4238c3605170b4d8ed7b4205c698b085019401@github.com/marca-development/blitz-monitoring-frontend.git"
