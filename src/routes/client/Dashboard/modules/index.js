6fe78b48da611957d7bf450e3488dc4f370add9e
reactivegeeks <software.dev0218@gmail.com>
2017-08-14 08:29:29 -0600

    add collapsable panels

--- src/routes/client/Dashboard/modules/index.js
+++ src/routes/client/Dashboard/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import privacyRemovalStats from './PrivacyRemovalStats';
 import privacyRemovalStatus from './privacyRemovalStatus';
 import notifications from './notifications';
+import hasData from './hasData';
 
 export default combineReducers({
   privacyRemovalStats,
   privacyRemovalStatus,
-  notifications
+  notifications,
+  hasData
 })
