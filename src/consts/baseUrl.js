de10801d5312439f46ff51717463e331be2bfb6c
reactivegeeks <software.dev0218@gmail.com>
2017-09-07 09:31:58 -0600

    add staging deploy and add staging base url

--- src/consts/baseUrl.js
+++ src/consts/baseUrl.js
@@ -1,6 +1,7 @@
 const BASE_URL = {
   'development': 'http://localhost:3000',
   'production' : 'https://api.clickadilly.com',
+  'staging'    : 'http://staging-app.clickadilly.com:3000',
   'test'       : 'http://localhost:3000'
 }
 
