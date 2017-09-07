2cf355eb32e2bae293a6c9330af4910ebc9cb801
reactivegeeks <software.dev0218@gmail.com>
2017-09-07 10:24:27 -0600

    updated config for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,14 @@
 set :application, "blitz-frontend"
 set :repo_url, "https://4e4238c3605170b4d8ed7b4205c698b085019401@github.com/marca-development/blitz-monitoring-frontend.git"
 
-set :branch, 'development'
+#set :branch, 'development'
+# set :branch, 'staging'
 
-set :deploy_to, '/home/ubuntu'
+#set :deploy_to, '/home/ubuntu'
 
+set :deploy_to, '/home/deploy'
+set :tmp_dir, "/home/deploy/tmp"
 
 set :keep_releases, 5
 
-after 'deploy:symlink:release', 'npm:deploy'
+# after 'deploy:symlink:release', 'npm:deploy'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,3 @@
 server "52.41.99.157", user: "ubuntu", roles: %w{app db web}
+set :branch, 'development'
+after 'deploy:symlink:release', 'npm:production_deploy'
