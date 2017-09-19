6c3a3546b7985fda69fb74e29affd4536e571a5b
reactivegeeks <software.dev0218@gmail.com>
2017-09-19 08:44:49 -0600

    change branch names where capistrano gets code
    staging => development branch
    development => master branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server "52.41.99.157", user: "ubuntu", roles: %w{app db web}
 
-set :branch, 'development'
+set :branch, 'master'
 set :deploy_to, '/home/ubuntu'
 after 'deploy:symlink:release', 'npm:production_deploy'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@ server "52.37.200.22", user: "deploy", roles: %w{app db web}
 
 set :deploy_to, '/home/deploy'
 set :tmp_dir, "/home/deploy/tmp"
-set :branch, 'staging'
+set :branch, 'development'
 after 'deploy:symlink:release', 'npm:staging_deploy'
