1b822a1a6756353613180bd54eedd06f8dba28e7
reactivegeeks <software.dev0218@gmail.com>
2017-11-30 09:44:46 -0700

    add raven error tracking

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,10 +1,14 @@
+import Raven from 'raven-js'
 import { applyMiddleware, compose, createStore } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
+import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-import { syncHistoryWithStore } from 'react-router-redux'
 import { useRouterHistory } from 'react-router'
 import thunk from 'redux-thunk'
 import makeRootReducer from './reducers'
+import createRavenMiddleware from 'raven-for-redux'
+
+const RAVEN_DSN = 'https://02166c399b7d4708ab5ff97d0e1dcb34@sentry.io/252851'
+Raven.config(RAVEN_DSN).install()
 
 const browserHistory = useRouterHistory(createBrowserHistory)({
   basename: __BASENAME__
@@ -14,7 +18,11 @@ export const store = (initialState = {}, history) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk, routerMiddleware(history)]
+  const middleware = [
+    thunk,
+    routerMiddleware(history),
+    createRavenMiddleware(Raven)
+  ]
 
   // ======================================================
   // Store Enhancers
