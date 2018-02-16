40b76698b6d0bdba601d32bc28b494b06e78f9d0
reactivegeeks <software.dev0218@gmail.com>
2018-02-16 09:38:57 -0700

    add tos to receipt modal

--- src/routes/Pages/TermsOfService/TermsOfService.js
+++ src/routes/Pages/TermsOfService/TermsOfService.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
-const TermsOfService = () => (
-  <div>
-    <p>
-      You must have Internet access and a current valid accepted payment method as indicated during sign-up ("Payment Method"), to use our service. We will begin billing your Payment Method for monthly membership fees upon enrollment. Your Payment Method will be authorized for up to approximately one month of service as soon as you register. TO CANCEL SEND AN EMAIL WITH YOUR ACCOUNT USERNAME AND PASSWORD TO HELP@INFOSWEEP.COM. We will continue to bill your Payment Method on a monthly basis for your membership fee until you cancel. You may cancel your membership at any time; however, there are no refunds or credits for partially used periods.
-    </p>
-    <h4>Billing</h4>
-    <p>
-      By starting your infosweep membership, you are expressly agreeing that we are authorized to charge your payment method with a monthly membership fee at the then current rate, and any other charges you may incur in connection with your use of the service to the Payment Method you provided during registration (or to a different Payment Method if you change your account information). Please note that prices and charges are subject to change with notice. As used in these Terms of Use, "billing" shall indicate either a charge or debit, as applicable, against your Payment Method. The membership fee will be billed at the beginning of your membership and each month thereafter unless and until you cancel your membership. We automatically bill your Payment Method each 30 days. By signing up for this service, you agree that membership charges are fully earned upon payment. For certain Payment Methods, the issuer of your Payment Method may charge you a foreign transaction fee or related charges. Check with your bank and credit card issuers for details. PAYMENTS ARE NONREFUNDABLE AND NO REFUNDS OR CREDITS FOR PARTIALLY USED PERIODS WILL BE ISSUED. We may change the fees and charges in effect, or add new fees and charges from time to time, but we will give you advance notice of these changes by email.
-    </p>
-    <h4>Ongoing Membership</h4>
-    <p>
-      Our membership, will continue month-to-month unless and until you cancel your membership or we terminate it. You must cancel your membership before it renews each month in order to avoid billing of the next month's membership fees to your Payment Method. We will bill the monthly membership fee to the Payment Method you provide to us during registration (or to a different Payment Method if you change your account information). Membership fees are fully earned upon payment.
-    </p>
-    <h4>Cancellation</h4>
-    <p>
-      You may cancel your membership at any time. WE DO NOT PROVIDE REFUNDS OR CREDITS FOR ANY PARTIAL-MONTH MEMBERSHIP PERIODS. To cancel, send an email to help@infosweep.com with your account name and password.
-    </p>
-    <p className='text-primary'>
-      If you have any questions or concerns regarding these terms or regarding the service, please contact our customer service team at (844) 641-7829
-    </p>
-  </div>
-)
+const TermsOfService = (props) => {
+
+  return (
+    <div>
+      <p>
+        You must have Internet access and a current valid accepted payment method as indicated during sign-up ("Payment Method"), to use our service. We will begin billing your Payment Method for monthly membership fees upon enrollment. Your Payment Method will be authorized for up to approximately one month of service as soon as you register. TO CANCEL SEND AN EMAIL WITH YOUR ACCOUNT USERNAME AND PASSWORD TO HELP@INFOSWEEP.COM. We will continue to bill your Payment Method on a monthly basis for your membership fee until you cancel. You may cancel your membership at any time; however, there are no refunds or credits for partially used periods.
+      </p>
+      <h4>Billing</h4>
+      <p>
+        By starting your infosweep membership, you are expressly agreeing that we are authorized to charge your payment method with a monthly membership fee at the then current rate, and any other charges you may incur in connection with your use of the service to the Payment Method you provided during registration (or to a different Payment Method if you change your account information). Please note that prices and charges are subject to change with notice. As used in these Terms of Use, "billing" shall indicate either a charge or debit, as applicable, against your Payment Method. The membership fee will be billed at the beginning of your membership and each month thereafter unless and until you cancel your membership. We automatically bill your Payment Method each 30 days. By signing up for this service, you agree that membership charges are fully earned upon payment. For certain Payment Methods, the issuer of your Payment Method may charge you a foreign transaction fee or related charges. Check with your bank and credit card issuers for details. PAYMENTS ARE NONREFUNDABLE AND NO REFUNDS OR CREDITS FOR PARTIALLY USED PERIODS WILL BE ISSUED. We may change the fees and charges in effect, or add new fees and charges from time to time, but we will give you advance notice of these changes by email.
+      </p>
+      <h4>Ongoing Membership</h4>
+      <p>
+        Our membership, will continue month-to-month unless and until you cancel your membership or we terminate it. You must cancel your membership before it renews each month in order to avoid billing of the next month's membership fees to your Payment Method. We will bill the monthly membership fee to the Payment Method you provide to us during registration (or to a different Payment Method if you change your account information). Membership fees are fully earned upon payment.
+      </p>
+      <h4>Cancellation</h4>
+      <p>
+        You may cancel your membership at any time. WE DO NOT PROVIDE REFUNDS OR CREDITS FOR ANY PARTIAL-MONTH MEMBERSHIP PERIODS. To cancel, send an email to help@infosweep.com with your account name and password.
+      </p>
+      <p className='text-primary'>
+        If you have any questions or concerns regarding these terms or regarding the service, please contact our customer service team at (844) 641-7829
+      </p>
+    </div>
+  )
+}
 
 export default TermsOfService;
