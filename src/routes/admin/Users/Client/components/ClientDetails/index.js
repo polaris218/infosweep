4134575e025cd4015a64b4d3b5540339be32ae09
reactivegeeks <software.dev0218@gmail.com>
2018-03-21 13:27:45 -0600

    add confirmation modal to account options

--- src/routes/admin/Users/Client/components/ClientDetails/index.js
+++ src/routes/admin/Users/Client/components/ClientDetails/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import infosweepApi from 'services/infosweepApi'
 
 import {
@@ -7,6 +8,7 @@ import {
   DropdownButton,
   MenuItem,
   ListGroupItem,
+  Modal,
   Label,
   Button
 } from 'components'
@@ -16,138 +18,186 @@ import { formatDate } from 'utils'
 
 import classes from '../user.scss'
 
-const ClientDetails = ({
-  accounts,
-  client,
-  showModal,
-  updateUserStatus,
-  handlePasswordReset
-}) => {
-  const _handleStatusUpdate = () => {
-    client.is_active
-      ? showModal('UPDATE_USER_STATUS')
-      : updateUserStatus(client)
+const ACCOUNT_OPTIONS_CONFIG = {
+  'passwordReset': 'send password reset email to client',
+  'introEmail': 'send intro email to client'
+}
+
+class ClientDetails extends Component {
+  constructor() {
+    super() 
+    this.state = {showModal: false}
+
+    this.hideModal = this.hideModal.bind(this)
+    this._handleStatusUpdate = this._handleStatusUpdate.bind(this)
+    this._handleAccountOptions = this._handleAccountOptions.bind(this)
+  }
+  
+  showAccountOptionsModal (target) {
+    this.setState({ showModal: true, target })
   }
 
-  const _handleAccountOptions = target => {
-    switch(target) {
+  hideModal () {
+    this.setState({ showModal: false })
+  }
+
+  _handleStatusUpdate () {
+    this.props.client.is_active
+      ? this.props.showModal('UPDATE_USER_STATUS')
+      : this.props.updateUserStatus(client)
+  }
+
+  _handleAccountOptions () {
+    this.hideModal()
+    switch(this.state.target) {
       case 'passwordReset':
-        handlePasswordReset()
+        this.props.handlePasswordReset()
         break;
       case 'introEmail':
-        const payload = { user_id: client.id }
+        const payload = { user_id: this.props.client.id }
         infosweepApi.patch('admin/api/emails/send-intro-email', payload)
         break;
     }
   }
 
-  return (
-    <Panel
-      header={
-        <h4 className='panel-title'>
-          Subscriber Details
-        </h4>
+  render () {
+    const {
+      accounts,
+      client,
+      showModal
+    } = this.props
+
+    const renderConfirmationModal = (
+      <Modal show={this.state.showModal}>
+        <Modal.Header>
+          <Modal.Title>
+            Please Confirm
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {`Are you sure you want to ${ACCOUNT_OPTIONS_CONFIG[this.state.target]}?`}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this._handleAccountOptions }>
+            Yes
+          </Button>
+          <Button onClick={this.hideModal}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+
+    return (
+      <Panel
+        header={
+          <h4 className='panel-title'>
+            Subscriber Details
+          </h4>
         }
-      footer={
-        <div>
-          <DropdownButton
-            bsStyle='primary'
-            title='Account options'
-            onSelect={target => { _handleAccountOptions(target) }}
-          >
-            <MenuItem
-              eventKey='passwordReset'
-            >
-              Send Password Reset
-            </MenuItem>
-            <MenuItem
-              eventKey='introEmail'
-            >
-              Send Intro Email
-            </MenuItem>
-          </DropdownButton>
-          <span className='pull-right'>
-            <Button
-              onClick={() => { showModal('USER', client) }}
+        footer={
+          <div>
+            <DropdownButton
               bsStyle='primary'
+              title='Account options'
+              onSelect={target => this.showAccountOptionsModal(target)}
+              id='accountOptions'
             >
-              <i className="fa fa-pencil"></i> Edit Subscriber
-            </Button>
-          </span>
-        </div>
-          }
-        >
-          <ListGroup className={classes.taskDetails}>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                User Id
-              </h5>
-              <div className={classes.detailsValue}>
-                {client.id}
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Status
-              </h5>
-              <div className={classes.detailsValue}>
-                <Toggle
-                  className="v-a-m m-r-1"
-                  checked={client.is_active}
-                  icons={false}
-                  onChange={_handleStatusUpdate}
-                />
-                <Label
-                  outline
-                  className='text-uppercase'
-                  bsStyle={client.is_active ? 'success' : 'danger'}>
-                  {client.is_active ? 'Active' : 'Inactive'}
-                </Label>
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Full Name
-              </h5>
-              <div className={classes.detailsValue}>
-                {client.fullName}
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Email
-              </h5>
-              <div className={classes.detailsValue}>
-                {client.email}
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Number of Accounts
-              </h5>
-              <div className={classes.detailsValue}>
-                {accounts.length}
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Date Assigned
-              </h5>
-              <div className={classes.detailsValue}>
-                {client.created_at}
-              </div>
-            </ListGroupItem>
-            <ListGroupItem className='flex-space-between'>
-              <h5 className={classes.detailsKey}>
-                Active until
-              </h5>
-              <div className={classes.detailsValue}>
-                {formatDate(client.active_until)}
-              </div>
-            </ListGroupItem>
-          </ListGroup>
-        </Panel>
-  )
+              <MenuItem
+                eventKey='passwordReset'
+              >
+                Send Password Reset
+              </MenuItem>
+              <MenuItem
+                eventKey='introEmail'
+              >
+                Send Intro Email
+              </MenuItem>
+            </DropdownButton>
+            <span className='pull-right'>
+              <Button
+                onClick={() => { showModal('USER', client) }}
+                bsStyle='primary'
+              >
+                <i className="fa fa-pencil"></i> Edit Subscriber
+              </Button>
+            </span>
+          </div>
+        }
+      >
+        { renderConfirmationModal }
+        <ListGroup className={classes.taskDetails}>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              User Id
+            </h5>
+            <div className={classes.detailsValue}>
+              {client.id}
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Status
+            </h5>
+            <div className={classes.detailsValue}>
+              <Toggle
+                className="v-a-m m-r-1"
+                checked={client.is_active}
+                icons={false}
+                onChange={this._handleStatusUpdate}
+              />
+              <Label
+                outline
+                className='text-uppercase'
+                bsStyle={client.is_active ? 'success' : 'danger'}>
+                {client.is_active ? 'Active' : 'Inactive'}
+              </Label>
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Full Name
+            </h5>
+            <div className={classes.detailsValue}>
+              {client.fullName}
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Email
+            </h5>
+            <div className={classes.detailsValue}>
+              {client.email}
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Number of Accounts
+            </h5>
+            <div className={classes.detailsValue}>
+              {accounts.length}
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Date Assigned
+            </h5>
+            <div className={classes.detailsValue}>
+              {client.created_at}
+            </div>
+          </ListGroupItem>
+          <ListGroupItem className='flex-space-between'>
+            <h5 className={classes.detailsKey}>
+              Active until
+            </h5>
+            <div className={classes.detailsValue}>
+              {formatDate(client.active_until)}
+            </div>
+          </ListGroupItem>
+        </ListGroup>
+      </Panel>
+    )
+  }
 }
 
 ClientDetails.propsTypes = {
